b78b4c38820e23d0088468b699d1b7c28b2f0d2f
jibo <jibo@outlook.com>
2019-10-09 08:40:54 +0800

    Refine code by shell check

--- add_font_sample.sh
+++ add_font_sample.sh
@@ -2,9 +2,9 @@
 curl -OL https://www.imagemagick.org/Usage/scripts/imagick_type_gen
 curl -OL https://raw.githubusercontent.com/adobe-fonts/source-han-sans/release/OTF/SimplifiedChinese/SourceHanSansSC-Normal.otf
 chmod a+x imagick_type_gen
-default=`gm convert -list font|grep type|awk '{print $2}'|sed -e 's/type-ghostscript/type/1'`
-sample=`./imagick_type_gen ${PWD}/SourceHanSansSC-Normal.otf|sed -e 's/<typemap>//1'|sed -e "s/<\/typemap>//1"|sed -e 's/<?xml version="1.0"?>//1'`
+default=$(gm convert -list font|grep type|awk '{print $2}'|sed -e 's/type-ghostscript/type/1')
+sample=$(./imagick_type_gen ${PWD}/SourceHanSansSC-Normal.otf|sed -e 's/<typemap>//1'|sed -e "s/<\/typemap>//1"|sed -e 's/<?xml version="1.0"?>//1')
 sed -i 's/<\/typemap>//1' ${default}
-echo ${sample} >> ${default}
-echo "</typemap>" >> ${default}
+echo "${sample}" >> "${default}"
+echo "</typemap>" >> "${default}"
 rm -rf imagick_type_gen
